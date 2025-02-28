fb7d3a2b648abbce29c48c588844dc72e9f41448
gong <ddddy0114@gmail.com>
2025-02-28 17:51:14 +0900

    add ar object page

---
+++ ar_sample_app/lib/object_page.dart
@@ -0,0 +1,102 @@
+import 'package:ar_flutter_plugin/ar_flutter_plugin.dart';
+import 'package:ar_flutter_plugin/datatypes/node_types.dart';
+import 'package:ar_flutter_plugin/managers/ar_anchor_manager.dart';
+import 'package:ar_flutter_plugin/managers/ar_location_manager.dart';
+import 'package:ar_flutter_plugin/managers/ar_object_manager.dart';
+import 'package:ar_flutter_plugin/managers/ar_session_manager.dart';
+import 'package:ar_flutter_plugin/models/ar_node.dart';
+import 'package:flutter/material.dart';
+import 'package:vector_math/vector_math_64.dart';
+
+class ObjectPage extends StatefulWidget {
+  const ObjectPage({super.key});
+
+  @override
+  State<ObjectPage> createState() => _ObjectPageState();
+}
+
+class _ObjectPageState extends State<ObjectPage> {
+  late ARSessionManager arSessionManager;
+  late ARObjectManager arObjectManager;
+  ARNode? localObjectNode;
+  ARNode? webObjectNode;
+  bool isAdd = false;
+
+  final sample =
+      "https://github.com/KhronosGroup/glTF-Sample-Models/blob/main/2.0/Duck/glTF-Binary/Duck.glb?raw=true";
+
+  final sample2 = "assets/chicken.glb";
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: ARView(onARViewCreated: onARViewCreated),
+      floatingActionButton: FloatingActionButton(
+        onPressed: onWebObjectAtButtonPressed,
+        child: Icon(isAdd ? Icons.remove : Icons.add),
+      ),
+    );
+  }
+
+  void onARViewCreated(
+      ARSessionManager arSessionManager,
+      ARObjectManager arObjectManager,
+      ARAnchorManager arAnchorManager,
+      ARLocationManager arLocationManager) {
+    this.arSessionManager = arSessionManager;
+    this.arObjectManager = arObjectManager;
+
+    this.arSessionManager.onInitialize(
+          showFeaturePoints: false,
+          showPlanes: true,
+          customPlaneTexturePath: "assets/triangle.png",
+          showWorldOrigin: true,
+          handleTaps: false,
+        );
+
+    this.arObjectManager.onInitialize();
+  }
+
+  Future onLocalObjectButtonPressed() async {
+    if (localObjectNode != null) {
+      arObjectManager.removeNode(localObjectNode!);
+      localObjectNode = null;
+    } else {
+      var newNode = ARNode(
+        type: NodeType.localGLTF2,
+        uri: 'assets/scene.gltf',
+        scale: Vector3(0.2, 0.2, 0.2),
+        position: Vector3(0.0, 0.0, 0.0),
+        rotation: Vector4(1.0, 0.0, 0.0, 0.0),
+      );
+      bool? didAddLocalNode = await arObjectManager.addNode(newNode);
+      localObjectNode = (didAddLocalNode!) ? newNode : null;
+    }
+  }
+
+  Future onWebObjectAtButtonPressed() async {
+    setState(() {
+      isAdd = !isAdd;
+    });
+
+    if (webObjectNode != null) {
+      arObjectManager.removeNode(webObjectNode!);
+      webObjectNode = null;
+    } else {
+      var newNode = ARNode(
+        type: NodeType.webGLB,
+        uri: sample2,
+        scale: Vector3(0.2, 0.2, 0.2),
+      );
+      bool? didAddWebNode = await arObjectManager.addNode(newNode);
+      webObjectNode = (didAddWebNode!) ? newNode : null;
+    }
+  }
+
+  @override
+  void dispose() {
+    arSessionManager.dispose();
+    super.dispose();
+  }
+}
